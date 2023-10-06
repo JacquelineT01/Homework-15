f9173401c0c48cf1e6c0016df0f769c5959070ea
Jacqueline <tiessijacqueline@yahoo.com>
2023-10-06 19:59:33 +0300

    Homework-05

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void celebrateBirthday(){
+        age++;
+    }
+
+    public void changeName(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
